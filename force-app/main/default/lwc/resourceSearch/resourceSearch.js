c1f032f2acfa0ac20776005ee9e0014f703d838c
Bilal <bilalshabazzb4b@gmail.com>
2023-02-22 17:21:45 -0500

    Clear Search Hide Qualified Roles

--- force-app/main/default/lwc/resourceSearch/resourceSearch.js
+++ force-app/main/default/lwc/resourceSearch/resourceSearch.js
@@ -1,8 +1,10 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 
-//import CLEARANCE_FIELD from '@salesforce/schema/Opportunity.StageName';
-//import CERTIFICATION_FIELD from '@salesforce/schema/Opportunity.Type';
+import CERTIFICATION_FIELD from '@salesforce/schema/Project_Assignment__c.Certifications__c';
+//import QUALIFIED_ROLES_FIELD from '@salesforce/schema/Work_Info__c.Qualified_Role_s__c';
+import SKILL_SET_FIELD from '@salesforce/schema/Project_Assignment__c.Skill_Set_Concentrations__c';
+
 //import getResources from '@salesforce/apex/ResourceSearchController.getResources';
 
 export default class ResourceSearch extends LightningElement 
@@ -10,11 +12,14 @@ export default class ResourceSearch extends LightningElement
     //MAIN VAR=================================================================
     @api searchJSON;    
     rowOffset = 0;
-    clearanceValue = "";
-    startDateValue = "";
-    endDateValue = "";
-    certValue="";
+    clearanceValue = "";   
+    certificationValue="";
+    qualifiedRolesValue="";
+    skillsValue="";
     availabilityValue=true;
+    certificationOptions;
+    qualifiedRolesOptions;
+    skillsOptions;
 
     //Set Clearance============================================================
     clearanceOptions = [
@@ -38,27 +43,136 @@ export default class ResourceSearch extends LightningElement
         {'label' : 'VA', 'value' : 'VA'},
     ];
 
+    //PICKLISTVALUES====================================================
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: CERTIFICATION_FIELD })    
+    certificationPicklistValue({ error, data })
+    {
+        //Get data from Picklist----------------------------------------------
+        if(data)
+        {
+            //Vars------------------------------------------------------------
+            let certificationHolder = [];
+            const certificationArr = data.values;
+
+            //Add Null Search Value--------------------------------------------
+            certificationHolder.push({'label' : '--ANY--', 'value' : ''});
+
+            //Get label and values---------------------------------------------
+            for(let x of certificationArr)
+            {
+                certificationHolder.push({'label' : x.label, 'value' : x.value});                
+            }
+            
+            this.certificationOptions = certificationHolder; 
+            this.error = undefined;
+        }
+        else if(error)
+        {
+            this.error = error;
+            this.certificationOptions = undefined;
+        }
+    }
+
+    //PICKLISTVALUES====================================================
+    /*
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: QUALIFIED_ROLES_FIELD })    
+    qualifiedRolePicklistValue({ error, data })
+    {
+        //Get data from Picklist----------------------------------------------
+        if(data)
+        {
+            //Vars------------------------------------------------------------
+            let qualifiedRoleHolder = [];
+            const qualifiedRoleArr = data.values;
+
+            //Add Null Search Value--------------------------------------------
+            qualifiedRoleHolder.push({'label' : '--ANY--', 'value' : ''});
+
+            //Get label and values---------------------------------------------
+            for(let x of qualifiedRoleArr)
+            {
+                qualifiedRoleHolder.push({'label' : x.label, 'value' : x.value});                
+            }
+            
+            this.qualifiedRolesOptions = qualifiedRoleHolder; 
+            this.error = undefined;
+        }
+        else if(error)
+        {
+            this.error = error;
+            this.qualifiedRolesOptions = undefined;
+        }
+    }
+    */
+
+    //PICKLISTVALUES====================================================
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: SKILL_SET_FIELD })    
+    skillsPicklistValue({ error, data })
+    {
+        //Get data from Picklist----------------------------------------------
+        if(data)
+        {
+            //Vars------------------------------------------------------------
+            let skillsHolder = [];
+            const skillsArr = data.values;
+
+            //Add Null Search Value--------------------------------------------
+            skillsHolder.push({'label' : '--ANY--', 'value' : ''});
+
+            //Get label and values---------------------------------------------
+            for(let x of skillsArr)
+            {
+                skillsHolder.push({'label' : x.label, 'value' : x.value});                
+            }
+            
+            this.skillsOptions = skillsHolder; 
+            this.error = undefined;
+        }
+        else if(error)
+        {
+            this.error = error;
+            this.skillsOptions = undefined;
+        }
+    }
+
     //Clearance Value Changed===========================================
     clearanceChange(event)
     {
         this.clearanceValue = event.detail.value;
     }
 
-    startDateChange(event)
+    //Certification Value Changed===========================================
+    certificationChange(event)
     {
-        this.startDateValue = event.detail.value;
+        this.certificationValue = event.detail.value;
     }
 
-    endDateChange(event)
+    /*
+    //Qualified Roles Value Changed===========================================
+    qualifiedRolesChange(event)
     {
-        this.endDateValue = event.detail.value;
-    }
+        this.qualifiedRolesValue = event.detail.value;
+    }*/
 
+    //Skills Value Changed====================================================
+    skillsChange(event)
+    {
+        this.skillsValue = event.detail.value;
+    }
+    
+    //Qualified Roles Value Changed===========================================
     availabilityChange(event)
     {
         this.availabilityValue = event.target.checked;
         console.log("avail:" + this.availabilityValue);
     }
+    
+    //Clear Search=============================================================
+    clearSearchClicked()
+    {
+
+    }
+
 
     //#########################################################################
 }
